58a6dc612f07d736267f6217dc35692193f32e5c
YazdanPlug <73750616+YazdanPlug@users.noreply.github.com>
2021-01-09 19:54:09 +0330

    Update Main.java

--- src/Main/iR.yzpllug.authui/Main.java
+++ src/Main/iR.yzpllug.authui/Main.java
@@ -1,21 +1,21 @@
-package iR.yzplug.authui;
+package iR.yzplug.AF;
 
 import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerFormRespondedEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
-import cn.nukkit.form.element.Element;
-import cn.nukkit.form.element.ElementButton;
 import cn.nukkit.form.element.ElementInput;
 import cn.nukkit.form.element.ElementLabel;
 import cn.nukkit.form.window.FormWindowCustom;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
-import SimpleAuth.SimpleAuth;
 
 public class Main extends PluginBase implements Listener {
+	
 	private Plugin auth;
+	
 	@Override
 	public void onEnable(){
 		
@@ -27,29 +27,45 @@ public class Main extends PluginBase implements Listener {
 			this.getLogger().error(TextFormat.RED + "Plugin AdvancedAuth is not found. please downloading this plugin and using this");
 		}
 	}
+	
 	@EventHandler
-	public void onPlaerJoin(PlayerJoinEvent event){
-		Player p = event.getPlayer();
-		// Register form
-		ElementInput password = new ElementInput("your password : ");
-		ElementInput confirmPassword = new ElementInput("confirm password : ");
-		
-		FormWindowCustom register = new FormWindowCustom("Register");
-		register.addElement(new ElementLabel("Welcome to this server, please registering in server"));
-		register.addElement(password);
-		register.addElement(confirmPassword);
-		
-		FormWindowCustom login = new FormWindowCustom("Login");
-		login.addElement(new ElementLabel("Welcome back to this server, please login in server"));
-		login.addElement(password);
-		if(!(p.hasPlayedBefore())){
+	public void onJoin(PlayerJoinEvent e){
+		// Forms :
+			// Object forms
+			ElementInput password = new ElementInput("your password : ");
+			ElementInput confirmPassword = new ElementInput("confirm password : ");
+			// Register forms
+			FormWindowCustom register = new FormWindowCustom("REGISTER");
+			register.addElement(new ElementLabel("Welcome to this server, please registering in server"));
+			register.addElement(password);
+			register.addElement(confirmPassword);
+			// login forms
+			FormWindowCustom login = new FormWindowCustom("LOGIN");
+			login.addElement(password);
+				
+		  Player p = e.getPlayer();
+		  
+		  if(!(p.hasPlayedBefore())){
+			        	p.showFormWindow(register);
+			        } else {
+			        p.showFormWindow(login);	
+			        }       
+	}
+		@EventHandler
+	    public void onResponse(PlayerFormRespondedEvent event) {
 			
-			p.showFormWindow(register);
-			getServer().dispatchCommand(p, "register" + password.getText() + " " + confirmPassword.getText());
-		} else {
-			
-			p.showFormWindow(login);
-		}
-		
+	        if (!(event.getWindow() instanceof FormWindowCustom)) return;
+	        FormWindowCustom fw = (FormWindowCustom) event.getWindow();
+	        
+	        if (fw.getTitle().equals("LOGIN")) {
+	            String pass =  fw.getResponse().getInputResponse(0);
+	            getServer().dispatchCommand(event.getPlayer(), "/login " + pass);
+	            
+	        } else if (fw.getTitle().equals("REGISTER")) {
+	            String pass = fw.getResponse().getInputResponse(0);
+	            String cpass = fw.getResponse().getInputResponse(1);
+	            getServer().dispatchCommand(event.getPlayer(), "/register " + pass + " "+ cpass);
+	        
+	    }
 	}
 }
